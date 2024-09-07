116606e76a653c7597249067093774b68926e51d
EPAFRA <enockepafra141@gmail..com>
2024-09-07 17:29:14 +0300

    rq

--- Blogs_project/settings.py
+++ Blogs_project/settings.py
@@ -81,11 +81,11 @@ WSGI_APPLICATION = 'Blogs_project.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.mysql',
-        DATABASE_NAME:os.getenv('DATABASE_NAME'),
-        DATABASE_USER : os.getenv('DATABASE_USER'),  
-        DATABASE_PASSWORD: os.getenv('DATABASE_PASSWORD'),  
-        DATABASE_HOST: os.getenv('DATABASE_HOST'), 
-        DATABASE_PORT: os.getenv('DATABASE_PORT')
+       ' DATABASE_NAME':os.getenv('DATABASE_NAME'),
+        'DATABASE_USER' : os.getenv('DATABASE_USER'),  
+        'DATABASE_PASSWORD': os.getenv('DATABASE_PASSWORD'),  
+       ' DATABASE_HOST': os.getenv('DATABASE_HOST'), 
+        'DATABASE_PORT': os.getenv('DATABASE_PORT')
     }
 }
 
